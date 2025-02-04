3e87ccf860fc00a12c41d44ab262d3c273fa35df
alabbar <abdullahalabbar920@gmail.com>
2025-02-04 23:19:41 +0400

    Add files via upload

---
+++ Lab2/strings1.rs
@@ -0,0 +1,9 @@
+// Fixed the return type by converting the string literal to a String
+fn current_favorite_color() -> String {
+    "blue".to_string()
+}
+
+fn main() {
+    let answer = current_favorite_color();
+    println!("My current favorite color is {answer}");
+}
