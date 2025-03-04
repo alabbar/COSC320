82118b58bca03f0c56c5192f66df98d8f0e9c1f4
alabbar <abdullahalabbar920@gmail.com>
2025-03-04 10:00:31 +0400

    Add files via upload

---
+++ Lab6/tests1.rs
@@ -0,0 +1,27 @@
+// Tests are important to ensure that your code does what you think it should
+// do.
+
+fn is_even(n: i64) -> bool {
+    n % 2 == 0
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO: Import `is_even`. You can use a wildcard to import everything in
+    // the outer module.
+
+    #[test]
+    fn you_can_assert() {
+        // TODO: Test the function `is_even` with some values.
+        let result = is_even(2);
+        let result2 = !is_even(3);
+        assert!(result);
+        assert!(result2);
+    }
+}
---
+++ Lab6/tests2.rs
@@ -0,0 +1,27 @@
+// Calculates the power of 2 using a bit shift.
+// `1 << n` is equivalent to "2 to the power of n".
+fn power_of_2(n: u8) -> u64 {
+    1 << n
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn you_can_assert_eq() {
+        // TODO: Test the function `power_of_2` with some values.
+        let a = power_of_2(1);
+        let b = power_of_2(2);
+        let c = power_of_2(3);
+        let d = power_of_2(4);
+        assert_eq!(a, 2);
+        assert_eq!(b, 4);
+        assert_eq!(c, 8);
+        assert_eq!(d, 16);
+    }
+}
